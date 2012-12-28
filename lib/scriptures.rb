87870639008b47ae737eaedb8fe574fdbdace778
Luke Holder <lukemh@gmail.com>
2012-12-28 19:21:38 +0800

    fixed

---
+++ lib/scriptures.rb
@@ -0,0 +1,13 @@
+require "data_mapper"
+
+require "scriptures/version"
+require "scriptures/setup"
+
+require "scriptures/models/volume"
+require "scriptures/models/book"
+require "scriptures/models/chapter"
+require "scriptures/models/verse"
+
+module Scriptures
+
+end
